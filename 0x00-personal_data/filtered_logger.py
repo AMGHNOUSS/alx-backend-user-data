6810f3a9e1287b0bb15a112c268bcaf59ffe8025
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-11-23 23:52:21 +0100

    Create Function filtred_datum()

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
